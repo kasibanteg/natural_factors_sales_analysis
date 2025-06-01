1e7330abd614bd60ee5a291ec7ca5f331f3c66f1
Geoffrey Kasibante <44870409+kasibanteg@users.noreply.github.com>
2025-06-01 07:14:13 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -142,4 +142,4 @@ app.layout = dbc.Container([
 
 if __name__ == '__main__':
     app.run(debug=True)
-app.run_server(host='0.0.0.0', port=int(os.environ.get("PORT", 8050)))
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
